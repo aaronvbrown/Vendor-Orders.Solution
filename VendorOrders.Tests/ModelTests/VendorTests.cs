ecf2da5da2716e2384503e0ec62f46ab0c49ec90
Aaron Brown <worldofaaron@hotmail.com>
2023-09-23 13:38:19 -0700

    Pass Vendor Name Tests

--- VendorOrders.Tests/ModelTests/VendorTests.cs
+++ VendorOrders.Tests/ModelTests/VendorTests.cs
@@ -11,7 +11,17 @@ public class VendorTests
   [TestMethod]
   public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
   {
-    Vendor newVendor = new Vendor("test vendor");
+    Vendor newVendor = new Vendor("I Buy Pastries");
     Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+  }
+
+  [TestMethod]
+  public void OnNewVendor_NewVendorNameShouldEqual_name()
+  {
+    string newName = "I Buy Pastries";
+    string badName = "I Don't Buy Pastries";
+    Vendor newVendor = new Vendor(newName);
+    Assert.AreNotEqual(newVendor.Name, badName);
+    Assert.AreEqual(newVendor.Name, newName);
   }
 }
