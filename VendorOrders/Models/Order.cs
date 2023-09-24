033a776a7feb0a844988bd34479b4a70a37ccd9a
Aaron Brown <worldofaaron@hotmail.com>
2023-09-24 08:53:23 -0700

    add Order Class

---
+++ VendorOrders/Models/Order.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System;
+
+namespace VendorOrders.Models
+{
+  public class Order
+  {
+    public string Title { get; set; }
+    private static List<Order> _instances = new List<Order> { };
+
+    public string Description( get; set; )
+    
+    public decimal Price( get; set; )
+
+    public DateTime Date( get; set; ) 
+
+    public Order( string title, string description, decimal price )
+    {
+      Title = title;
+      _instances.Add(this);
+      Description = description;
+      Price = price;
+      Date = DateTime.Today;
+    } 
+
+
+  }
+}
